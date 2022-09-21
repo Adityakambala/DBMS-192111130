21cbf0bde2609c1ae17d4fb4b880ec8f5f5f5f68
Adityakambala <113966556+Adityakambala@users.noreply.github.com>
2022-09-21 10:18:43 +0530

    Delete inserting data in table.sql

--- inserting data in table.sql
+++
@@ -1,9 +0,0 @@
-use coffee_store;
-select*from product;
-insert into <tablename>(<column1>,<column2>,<column3>)
-values('value1','value2','value3');
-insert into product (name,price,coffee_origin)
-values('espresso',2.50,'brazil');
-update product
-set coffee_origin='srilanka'
-where id='4';
